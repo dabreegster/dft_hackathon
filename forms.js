9e2e080df1ccabadd46e1a815e1d05e9d06fd432
Dustin Carlino <dabreegster@gmail.com>
2022-12-18 14:55:44 +0000

    Remove unused form code

--- forms.js
+++ forms.js
@@ -13,39 +13,3 @@ export function dropdown(props, key, label, values) {
   html += `</div>`;
   return html;
 }
-
-export function radio(props, id, key, values) {
-  var html = `<div style="display: flex; justify-content: space-between;">`;
-  for (const val of values) {
-    const checked = props[key] == val ? "checked" : "";
-    html += `<input type="radio" id="${val}" name="${id}" ${checked}>`;
-    const capitalized = val.charAt(0).toUpperCase() + val.slice(1);
-    html += `<label for="${val}">${capitalized}</label>`;
-  }
-  html += `</div>`;
-  return html;
-}
-
-export function checkbox(props, key, label) {
-  const checked = props[key] ? "checked" : "";
-  return `<input type="checkbox" id="${key}" ${checked}>
-		      <label for="${key}">${label}</label><br>`;
-}
-
-export function checkboxes(props, prefix, labels) {
-  var result = ``;
-  var idx = 0;
-  for (const label of labels) {
-    const key = prefix + String.fromCharCode("a".charCodeAt(0) + idx);
-    result += checkbox(props, key, label);
-    idx += 1;
-  }
-  return result;
-}
-
-export function checkbox_ids(prefix, num) {
-  return Array.from(
-    { length: num },
-    (_, i) => prefix + String.fromCharCode("a".charCodeAt(0) + i)
-  );
-}
