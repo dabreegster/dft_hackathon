054bdeae280b01ed6462b9b60cac36c0c4045fc3
Dustin Carlino <dabreegster@gmail.com>
2022-12-18 15:04:25 +0000

    Split out some chart code

--- app.js
+++ app.js
@@ -1,116 +1,10 @@
+import { makeChartsCon } from "./area_summary.js";
 import { dropdown } from "./forms.js";
 import { geojsonLength } from "./deps/geojson-length.js";
 
 ("use strict");
 
 export class App {
-  #makeChartsCon(sub) {
-    var subcar = [
-      sub.overall_car,
-      sub.Business_car,
-      sub.Education_car,
-      sub.Shopping_car,
-      sub.Visit_friends_at_private_home_car,
-      sub.Entertain_public_activity_car,
-    ];
-
-    var subwalk = [
-      sub.overall_walk,
-      sub.Business_walk,
-      sub.Education_walk,
-      sub.Shopping_walk,
-      sub.Visit_friends_at_private_home_walk,
-      sub.Entertain_public_activity_walk,
-    ];
-
-    var subcycle = [
-      sub.overall_cycling,
-      sub.Business_cycling,
-      sub.Education_cycling,
-      sub.Shopping_cycling,
-      sub.Visit_friends_at_private_home_cycling,
-      sub.Entertain_public_activity_cycling,
-    ];
-
-    var subpt = [
-      sub.overall_pt,
-      sub.Business_pt,
-      sub.Education_pt,
-      sub.Shopping_pt,
-      sub.Visit_friends_at_private_home_pt,
-      sub.Entertain_public_activity_pt,
-    ];
-
-    // Connectivity Chart
-    if (this.conChart) {
-      this.conChart.destroy();
-    }
-
-    var conctx = document.getElementById("conChart").getContext("2d");
-
-    conChart = new Chart(conctx, {
-      type: "bar",
-      data: {
-        labels: [
-          "Overall",
-          "Business",
-          "Education",
-          "Shopping",
-          "Friends",
-          "Entertainment",
-        ],
-        datasets: [
-          {
-            label: "Car",
-            data: subcar,
-            backgroundColor: "rgba(255, 99, 132, 0.8)",
-            borderColor: "rgba(255, 99, 132, 1)",
-            borderWidth: 1,
-          },
-          {
-            label: "Walk",
-            data: subwalk,
-            backgroundColor: "rgba(132, 99, 255, 0.8)",
-            borderColor: "rgba(132, 99, 255, 1)",
-            borderWidth: 1,
-          },
-          {
-            label: "Bicycle",
-            data: subcycle,
-            backgroundColor: "rgba(99, 255, 13, 0.8)",
-            borderColor: "rgba(99, 255, 13, 1)",
-            borderWidth: 1,
-          },
-          {
-            label: "Public Transport",
-            data: subpt,
-            backgroundColor: "rgba(152,78,163, 0.8)",
-            borderColor: "rgba(152,78,163, 1)",
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        legend: { position: "bottom" },
-        scales: {
-          yAxes: [
-            {
-              scaleLabel: {
-                display: true,
-                labelString: "Connectivity Score",
-              },
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
-        responsive: true,
-        maintainAspectRatio: false,
-      },
-    });
-  }
-
   constructor() {
     this.map = new maplibregl.Map({
       container: "map",
@@ -164,8 +58,7 @@ export class App {
 
         if (f.length == 1) {
           modal.style.display = "block";
-          var sub = e.features[0].properties;
-          this.#makeChartsCon(sub);
+          makeChartsCon(e.features[0].properties);
         }
       }
     });
---
+++ area_summary.js
@@ -0,0 +1,101 @@
+export function makeChartsCon(sub) {
+  const subcar = [
+    sub.overall_car,
+    sub.Business_car,
+    sub.Education_car,
+    sub.Shopping_car,
+    sub.Visit_friends_at_private_home_car,
+    sub.Entertain_public_activity_car,
+  ];
+
+  const subwalk = [
+    sub.overall_walk,
+    sub.Business_walk,
+    sub.Education_walk,
+    sub.Shopping_walk,
+    sub.Visit_friends_at_private_home_walk,
+    sub.Entertain_public_activity_walk,
+  ];
+
+  const subcycle = [
+    sub.overall_cycling,
+    sub.Business_cycling,
+    sub.Education_cycling,
+    sub.Shopping_cycling,
+    sub.Visit_friends_at_private_home_cycling,
+    sub.Entertain_public_activity_cycling,
+  ];
+
+  const subpt = [
+    sub.overall_pt,
+    sub.Business_pt,
+    sub.Education_pt,
+    sub.Shopping_pt,
+    sub.Visit_friends_at_private_home_pt,
+    sub.Entertain_public_activity_pt,
+  ];
+
+  const conctx = document.getElementById("conChart").getContext("2d");
+
+  new Chart(conctx, {
+    type: "bar",
+    data: {
+      labels: [
+        "Overall",
+        "Business",
+        "Education",
+        "Shopping",
+        "Friends",
+        "Entertainment",
+      ],
+      datasets: [
+        {
+          label: "Car",
+          data: subcar,
+          backgroundColor: "rgba(255, 99, 132, 0.8)",
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+        },
+        {
+          label: "Walk",
+          data: subwalk,
+          backgroundColor: "rgba(132, 99, 255, 0.8)",
+          borderColor: "rgba(132, 99, 255, 1)",
+          borderWidth: 1,
+        },
+        {
+          label: "Bicycle",
+          data: subcycle,
+          backgroundColor: "rgba(99, 255, 13, 0.8)",
+          borderColor: "rgba(99, 255, 13, 1)",
+          borderWidth: 1,
+        },
+        {
+          label: "Public Transport",
+          data: subpt,
+          backgroundColor: "rgba(152,78,163, 0.8)",
+          borderColor: "rgba(152,78,163, 1)",
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      legend: { position: "bottom" },
+      scales: {
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: "Connectivity Score",
+            },
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+    },
+  });
+}
