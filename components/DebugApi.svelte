978418a88bb3cd48f4e186de7c33b3b220be2a32
Dustin Carlino <dabreegster@gmail.com>
2023-01-04 16:08:48 +0000

    Fix the modal debug popups

--- components/DebugApi.svelte
+++ components/DebugApi.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { Modal } from "carbon-components-svelte";
+  import { Modal, TextArea } from "carbon-components-svelte";
 
   export let requestJson;
   export let responseJson;
@@ -17,19 +17,19 @@
   }
 </script>
 
-<Modal passiveModal modalHeading="Request JSON" bind:openRequest>
-  <pre>{JSON.stringify(requestJson)}</pre>
+<Modal passiveModal modalHeading="Request JSON" bind:open={openRequest}>
+  <TextArea
+    readonly
+    rows="20"
+    value={JSON.stringify(requestJson, null, "  ")}
+  />
 </Modal>
-<Modal passiveModal modalHeading="Response JSON" bind:openResponse>
-  <pre>{JSON.stringify(responseJson)}</pre>
+<Modal passiveModal modalHeading="Response JSON" bind:open={openResponse}>
+  <TextArea
+    readonly
+    rows="20"
+    value={JSON.stringify(responseJson, null, "  ")}
+  />
 </Modal>
 <button type="button" on:click={toggleRequest}>Debug API request</button>
 <button type="button" on:click={toggleResponse}>Debug API response</button>
-
-<!-- TODO The modals aren't working? -->
-{#if openRequest}
-  <pre>{JSON.stringify(requestJson)}</pre>
-{/if}
-{#if openResponse}
-  <pre>{JSON.stringify(responseJson)}</pre>
-{/if}
